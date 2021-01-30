e1106362a0d57aa7b6d38dbaa7943514a25caee7
darkmuffle <redaib@DESKTOP-EHHQM65.localdomain>
2021-01-30 20:20:06 +0100

    sending back user reply

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 module.exports = require('./src');
+
+async function App(context) {
+    if (context.event.isText) {
+      await context.sendText(`received the text message: ${context.event.text}`);
+    }
+  }
