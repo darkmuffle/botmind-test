16f42eefb7a94283ebaf325e2df14fea7cc0fafa
darkmuffle <redaib@DESKTOP-EHHQM65.localdomain>
2021-01-30 20:54:08 +0100

    m

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = async function App(context) {
   if (context.event.isText ) {
     // Case where the sent media is the known string
     if (context.event.text === 'comment vas-tu ?'){
-      await context.sendText('Très bien et vous ?' ,  {
+      await context.sendText('Très bien ?' ,  {
         quickReplies: [
           {
             contentType: 'text',
