a2cd669ddbf802b06125cfa2e8b5005314f1bace
darkmuffle <redaib@DESKTOP-EHHQM65.localdomain>
2021-01-30 21:07:31 +0100

    final commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 module.exports = async function App(context) {
 
-  if (context.event.isText ) {
-    // Case where the sent media is the known string
-    if (context.event.text === 'comment vas-tu ?'){
-      await context.sendText('Très bien ?' ,  {
-        quickReplies: [
-          {
-            contentType: 'text',
-            title: 'Je vais bien, merci.',
-            payload: '<DEVELOPER_DEFINED_PAYLOAD>',
-          },
-          {
-            contentType: 'text',
-            title: 'Non, ça ne va pas.',
-            payload: '<DEVELOPER_DEFINED_PAYLOAD>',
-          },
-        ],
-      })
-    }
-    // Image media event
-    else if (context.event.isImage) {
-      await context.sendText('Je ne sais pas traiter ce type de demande')
-    }
-    // In any other case sending back what the user sent. 
-    else {
-      await context.sendText(context.event.text)
-    }
-  }
+// Case where the sent media is the known string
+if (context.event.text === 'comment vas-tu ?'){
+  await context.sendText('Très bien ?' ,  {
+  // Sending back 2 quick reply options
+    quickReplies: [
+      {
+        contentType: 'text',
+        title: 'Je vais bien, merci.',
+        payload: '<DEVELOPER_DEFINED_PAYLOAD>',
+      },
+      {
+        contentType: 'text',
+        title: 'Non, ça ne va pas.',
+        payload: '<DEVELOPER_DEFINED_PAYLOAD>',
+      },
+    ],
+  })
+}
+  // Image media event
+else if (context.event.isImage) {
+  await context.sendText('Je ne sais pas traiter ce type de demande')
+}
+// In any other case sending back what the user sent. 
+else {
+  await context.sendText(context.event.text)
+}
 
 
 };
